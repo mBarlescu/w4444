22d6710947577782e85fe493b324910cb04295a4
Mark Barlescu <barlescu92@gmail.com>
2018-11-14 05:35:42 +0000

    querrying data A GO

--- lookup_people.js
+++ lookup_people.js
@@ -16,16 +16,18 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT first_name FROM famouss_people where first_name='"+[myArgs]+"'", (err, result) => {
+  console.log('Searching....')
+  client.query(`SELECT * FROM famouss_people where first_name='${myArgs}'`, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log("Total Number of Rows  ",result.rows.length);
-    //console.log(result.rows); //output: 1
-    for(var i = 0; i < result.rows.length; i++){
-      console.log(result.rows[i].first_name);
-    }
+    console.log('RESULT',result);
+    console.log(`Found ${result.rowCount} person(s) by the name 'Paul':`);
+    result.rows.forEach(function (value, i){
+      console.log(`${value.id}: ${value.first_name} ${value.last_name}, born '${value.birthdate}'`);
+    })
 
     client.end();
   });
 });
+// first_name='"+[myArgs]+"'"
